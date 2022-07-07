4fd12287288193710bc582b77d789bbbe9409a41
Jonatan Sombié <jonatan.sombie@gmail.com>
2022-07-07 12:18:48 +0200

    initial commi

---
+++ README.md
@@ -0,0 +1,4 @@
+# nextauth_cognito
+
+DReAM package for creating amazon cognito user pool and app client configured
+for [nextauth](https://next-auth.js.org/providers/cognito).
---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  #  backend "s3" {}
+
+  required_providers {
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = "3.2.0"
+    }
+
+    aws = {
+      source  = "registry.terraform.io/hashicorp/aws"
+      version = "4.21.0"
+    }
+  }
+}
+
+provider "random" {}
+provider "aws" {}
+
+resource "random_pet" "pool_name" {}
+
+resource "aws_cognito_user_pool" "pool" {
+  name = random_pet.pool_name.id
+}
+
+resource "random_pet" "client_name" {}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name = random_pet.client_name.id
+
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  generate_secret              = true
+  callback_urls                = ["http://localhost:3000/api/auth/callback/cognito"]
+  allowed_oauth_flows          = ["code"]
+  allowed_oauth_scopes         = ["email", "openid", "profile"]
+  supported_identity_providers = ["COGNITO"]
+}
+
+output "COGNITO_CLIENT_ID" {
+  sensitive = true
+  value     = aws_cognito_user_pool_client.client.id
+}
+
+output "COGNITO_CLIENT_SECRET" {
+  sensitive = true
+  value     = aws_cognito_user_pool_client.client.client_secret
+}
+
+output "COGNITO_ISSUER" {
+  sensitive = true
+  value     = "https://${aws_cognito_user_pool.pool.endpoint}"
+}
